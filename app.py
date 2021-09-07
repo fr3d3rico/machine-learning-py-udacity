315c6626e5489d63ff44fc30bfbf0c58f3958e92
Fred <frederico.a.soares@gmail.com>
2021-09-07 14:13:35 -0300

    updated based https://knowledge.udacity.com/questions/253413

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    dummyVar =""
+    html = f"<h3>Sklearn Prediction Home</h3>{dummyVar}"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
