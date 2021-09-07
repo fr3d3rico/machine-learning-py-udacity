a7f992bfc99b670eafb9bb2d8e8d72596e97ca68
Fred <frederico.a.soares@gmail.com>
2021-09-07 14:28:22 -0300

    returned app.py source

--- app.py
+++ app.py
@@ -20,8 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    dummyVar =""
-    html = f"<h3>Sklearn Prediction Home</h3>{dummyVar}"
+    html = f"<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
